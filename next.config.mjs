420f8f00e4f1a4cf3747541f7e3ff983c2fe915d
Kael <119458475+paradisdunord@users.noreply.github.com>
2026-05-02 13:25:30 +0000

    make sure it work with pages  ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/towerdefense1',
+  assetPrefix: '/towerdefense1/',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
